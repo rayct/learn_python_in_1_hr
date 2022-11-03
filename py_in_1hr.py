7c85c05f19aeb529c2b962499a86bf14ec01d1e1
Raymond Turner <ray@Raymonds-MacBook-Pro.local>
2022-11-03 16:25:19 +0000

    first commit

---
+++ py_in_1hr.py
@@ -0,0 +1,17 @@
+
+#1 Patients Records
+# a=input('Give new patient’s name:')
+# b=int(input('Give new patient’s age:'))
+# print(a, 'is' ,b, 'years old and a new patient!')
+
+
+
+#2 Calculator
+# first_number = input("First: ")
+# second_number = input("Second: ")
+# add = float(first_number) + int(second_number)
+# print("Sum: " + str(add))
+
+
+# 3
+
